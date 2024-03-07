c69099f2b3325854173e8eb4566edb28c2363a6d
juliia <poshtaklon@gmail.com>
2024-03-07 15:48:10 +0200

    delete try from add_phone

--- goit-algo-06.py
+++ goit-algo-06.py
@@ -27,11 +27,10 @@ class Record:
         self.phones = []
 
     def add_phone(self, phone):
-        try:
-            ph = Phone(phone)
-            self.phones.append(ph)
-        except ValueError:
-            return ph
+        ph = Phone(phone)
+        self.phones.append(ph)
+        return ph
+       
 
     def find_phone(self, phone):
         for ph in self.phones:
@@ -48,14 +47,10 @@ class Record:
     def edit_phone(self, old_phone: str, new_phone: str):
         ph = self.find_phone(old_phone)
         if ph:
+            self.add_phone(new_phone)
             self.remove_phone(old_phone)
-            try:
-                self.add_phone(new_phone)
-            except ValueError:
-                return "Phone number invalid."
         else:
-            return "Phone edited."
-        return "Phone not found."
+            raise ValueError
 
 
     def __str__(self):
@@ -76,33 +71,33 @@ class AddressBook(UserDict):
 
     # Створення нової адресної книги
 
-#
-# book = AddressBook()
-#
-# # Створення запису для John
-# john_record = Record("John")
-# john_record.add_phone("1234567890")
-# john_record.add_phone("5555555555")
-# # Додавання запису John до адресної книги
-# book.add_record(john_record)
-#
-# # Створення та додавання нового запису для Jane
-# jane_record = Record("Jane")
-# jane_record.add_phone("9876543210")
-# book.add_record(jane_record)
-#
-# # Виведення всіх записів у книзі
-# for name, record in book.data.items():
-#     print(record)
-#     # Знаходження та редагування телефону для John
-# john = book.find("John")
-# john.edit_phone("1234567890", "1112223333")
-#
-# print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
-#
-# # Пошук конкретного телефону у записі John
-# found_phone = john.find_phone("5555555555")
-# print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
-#
-# # Видалення запису Jane
-# book.delete("Jane")
+
+book = AddressBook()
+
+# Створення запису для John
+john_record = Record("John")
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
+# Додавання запису John до адресної книги
+book.add_record(john_record)
+
+# Створення та додавання нового запису для Jane
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+book.add_record(jane_record)
+
+# Виведення всіх записів у книзі
+for name, record in book.data.items():
+    print(record)
+    # Знаходження та редагування телефону для John
+john = book.find("John")
+john.edit_phone("1234567890", "1112223333")
+
+print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+
+# Пошук конкретного телефону у записі John
+found_phone = john.find_phone("5555555555")
+print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
+
+# Видалення запису Jane
+book.delete("Jane")
